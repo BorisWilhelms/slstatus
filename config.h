5c2119093ba36e03eeb1de6e8d270ecd9d333f0c
Boris Wilhelms <mail@wille-zone.de>
2021-09-21 08:58:07 +0200

    Updates config

--- config.h
+++ config.h
@@ -64,6 +64,8 @@ static const char unknown_str[] = "n/a";
  * wifi_essid          WiFi ESSID                      interface name (wlan0)
  */
 static const struct arg args[] = {
-	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	/* function     format          argument */
+	{ battery_perc, "🔋 %s\%",      "BAT0"},
+	{ battery_perc, "🔋 %s\%",      "BAT0"},
+	{ datetime,     "%s",           "📅 %d.%m. 🕒 %H:%M" }
 };
